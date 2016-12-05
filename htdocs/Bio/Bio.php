607542f6383894332982e715499c151e20f29be3
Bettina93 <bettinahab@web.de>
2016-12-05 17:28:20 +0100

    Dateien

---
+++ htdocs/Bio/Bio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="de"> 
+<head> 
+	<title>Alleinunterhalter</title>
+	<meta name="author" content="Stefanie Eberhart, Bettina Haberstroh" />
+	<meta name="date" content="2016-11-20" />
+	<meta name="keywords" content="Musik"/>
+	<link type="text/css" rel="stylesheet" href="../stylesheet.css"/>
+</head>
+<body>
+	<?php
+	include("../nav.php");
+	?>
+	<p id="T-Ganz">Biographie, Fotos usw.<br/></p>
+</body>
+</html>
---
+++ htdocs/Home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="de"> 
+<head> 
+	<title>Mike and the Electronics</title>
+	<meta name="author" content="Stefanie Eberhart, Bettina Haberstroh" />
+	<meta name="date" content="2016-11-20" />
+	<meta name="keywords" content="Musik"/>
+	<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+</head>
+<body>
+	<?php
+	include("nav.php");
+	?>
+	<p id="Text">Hallo<br/>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+	<iframe src="https://www.youtube.com/embed/79DijItQXMM" allowfullscreen></iframe></p>
+	<div id="Sidebar"><p>Newsletter/Aktuelles</p></div>
+</body>
+</html>
