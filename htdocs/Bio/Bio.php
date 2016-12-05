083f5f62aa65c7eb118ec43db3deea9cea3143fd
Bettina93 <bettinahab@web.de>
2016-12-05 17:55:53 +0100

    Update Bio.php

--- htdocs/Bio/Bio.php
+++ htdocs/Bio/Bio.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="de"> 
 <head> 
-	<title>Alleinunterhalter</title>
+	<title>Mike and the Electronics</title>
 	<meta name="author" content="Stefanie Eberhart, Bettina Haberstroh" />
 	<meta name="date" content="2016-11-20" />
 	<meta name="keywords" content="Musik"/>
