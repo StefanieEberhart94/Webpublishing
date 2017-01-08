9a48d737a2b200d6b8af0c97597a10b5cd5dac76
StefanieEberhart94 <stefanie.eberhart@gmx.de>
2017-01-08 11:58:57 +0100

    Update Bio.php

--- htdocs/Bio/Bio.php
+++ htdocs/Bio/Bio.php
@@ -11,6 +11,19 @@
 	<?php
 	include("../nav.php");
 	?>
-	<p id="T-Ganz">Biographie, Fotos usw.<br/></p>
+	<p id="T-Ganz"> </br>Lebenslauf:<br/></br>
+	1963 Geburtsdatum</br>
+	1971 Beginn der musikalischen Ausbildung in der SK Binsdorf als Trompeter</br>
+	1977 Beginn mit E-Bass lernen+Mitspielen in verschiedenen Tanz & Unterhaltungsbands</br>
+	1981 Mitglied im Kreisverbandsjugendorchester ZAK</br>
+    	&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Aushilfe als Trompeter in verschiedenen Vereinen überregional</br>
+	1981-1983 Bassist in einer bekannten Bigband im Kreis Rottweil</br>
+	1984 -1985 Mitglied als Trompeter im Luftwaffenmusikkorps ll in Karlsruhe</br>
+	1985 -1988 Mitglied in verschiedenen Bands als Trompeter und Bassist</br>
+	1992-2000 Trompeter/ Bassist in einer überregional bekannten Gala- und Tanzband</br>
+	1996- heute Dirigent in verschiedenen Blasorchester</br>
+	seit 2012 Klavierunterricht</br></br>
+	Fehlt noch: Fotos
+	</p>
 </body>
 </html>
