7e9004719ffaf46d92b5aca8ff1ac996bea9cf82
Bettina93 <bettinahab@web.de>
2017-01-09 17:19:05 +0100

    Update Home.php

--- htdocs/Home.php
+++ htdocs/Home.php
@@ -11,15 +11,17 @@
 	<?php
 	include("nav.php");
 	?>
-	<p id="Text"> </br>Mike and the Electronics <br/>
+	<div id="Halb">
+	<p> </br>Mike and the Electronics <br/>
 	ist nicht nur Michael Eberhart als Alleinunterhalter, sondern auch verschiedene Besetzungen für Anlässe aller Art. </br>
 	Erfahren Sie auf den nachfolgenden Seiten mehr über Mike and the Electronics! </br>
 	Unterschrift der Band
 	<iframe src="https://www.youtube.com/embed/AGRjnC7IkIM" allowfullscreen></iframe>
 	</p>
+	</div>
 	<div id="Sidebar"><p>Newsletter </br>
 	Aktuelles</br></br>
-	<strong>Impressum</strong> </br>Mike and the Electronics</br>Michael Eberhart</br>Tel 07428/3417</br>
+	<span class="fett"> Impressum</span></br>Mike and the Electronics</br>Michael Eberhart</br>Tel 07428/3417</br>
 	michael.eberhart1@gmx.de</br>Bei Buchungen gelten unsere Allgemeinen Geschäftsbedingungen:</br>Datei siehe Tanz- und Showband Prestige</p>
 	</p></div>
 </body>
