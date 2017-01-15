74093c88a500d534ff64a4d7478c0bdb6e76c218
StefanieEberhart94 <stefanie.eberhart@gmx.de>
2017-01-15 16:29:29 +0100

    Update Musik.php

--- htdocs/Musik/Musik.php
+++ htdocs/Musik/Musik.php
@@ -1,19 +1,34 @@
-<!DOCTYPE html>
-<html lang="de"> 
-<head> 
-	<title>Alleinunterhalter</title>
-	<meta name="author" content="Stefanie Eberhart, Bettina Haberstroh" />
-	<meta name="date" content="2016-11-20" />
-	<meta name="keywords" content="Musik"/>
-	<link type="text/css" rel="stylesheet" href="../stylesheet.css"/>
 </head>
 <body>
 	<?php
 	include("../nav.php");
 	?>
+	
+	<!--Grafik Stilrichtungen-->
 	<div id="Ganz"><p id="Überschrift">Stilrichtungen:<p>
 	<img id="Grafik" src="../Bilder/Stilrichtungen.JPG" alt="Stilrichtungen">
-	</p>
+	</p></br></br>
+	
+	<!-- Text-->
+	<p id="Fett">1 Mann Entertainer - 7 Mann Galabesetzung</p>
+	<p>Oldies; Schlager; Charts; 80`s; 90`s; Funk & Soul; Latin; Rock</br> 
+	-->Zusätzlich buchbar sind 30-minütige Showblöcke aus :  Schlager; Pop und Neue Deutsche Welle</p>
+	<p id="Kursiv">Besonderheit: musikalische Umrahmung oder Begleitung von Veranstaltungen (z.B. Trauung in Kirche)</p></br></br>
+	<p id="Fett">3 - 5 Mann Jazzformation (ohne/mit Gesang)</p>
+	<p>Real Book; Latin</p>
+	<p id="Kursiv">Besonderheit: eigene Jazzversionen aus Pop/ Rock</p></br></br>
+	<p id="Fett">7 - 14 Mann Blasorchester (mit Gesang)</p>
+	<p>Musik von Alpenblech; Berthold Schick und seine allgäu 6; Ernst Mosch; Guido Henn; Norbert Gälle; ...</p>
+	<p id="Kursiv">Besonderheit: spielt auch in Wirtschaften, kleineren Räumlichkeiten ohne Anlage (max. 3 Stunden)</p></br></br>
+	<p id="Fett">15 Mann Bigband (mit Gesang)</p>
+	<p>Wir spielen alles vom Jazzklassiker bis zum aktuellen Hit im modernen Big Band Sound</p>
+	<p id="Kursiv">Besonderheit: großes Showprogramm mit Solisten und professionellem  Conferencier</p></br></br>
+	
+	<p id="Rahmen">Alle Formationen spielen mit entsprechend professioneller Beschallungs- und Lichtanlage</br> 
+		(von 20 – bis ca. 600 Personen - abhängig von den Örtlichkeiten)</p></br></br>
+	<p id="Farbe">Haben Sie das Richtige Gefunden?</p></br>
+	<p id="Mittig">Dann Buchen Sie uns!</p>
+
 	</div>
 </body>
 </html>
