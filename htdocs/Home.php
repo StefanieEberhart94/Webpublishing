ba6f63b96229d9b075743b3a292fd931f764f9d9
StefanieEberhart94 <stefanie.eberhart@gmx.de>
2017-01-15 16:29:54 +0100

    Update Home.php

--- htdocs/Home.php
+++ htdocs/Home.php
@@ -12,17 +12,59 @@
 	include("nav.php");
 	?>
 	<div id="Halb">
-	<p id="Überschrift">Mike and the Electronics</p>
-	<p>ist nicht nur Michael Eberhart als Alleinunterhalter, sondern auch verschiedene Besetzungen für Anlässe aller Art. </br>
-	Erfahren Sie auf den nachfolgenden Seiten mehr über Mike and the Electronics! </br>
-	Unterschrift der Band
-	<iframe src="https://www.youtube.com/embed/AGRjnC7IkIM" allowfullscreen></iframe>
-	</p>
+	<p id="Mittig">Hey lieber Bandsuchender!</p>
+	<p>Du bist zurzeit auf der Suche nach einer Band, die Deine Party oder Dein Fest musikalisch begleiten  soll, und die verschiedenste Musikstile professionell abdecken kann, damit die Veranstaltung zu einem vollen Erfolg wird?</br></br>
+	Eventuell suchst Du sogar eine Band die einen ganz speziellen Stil abdecken soll.</br></br>
+	Wir können Dir hier auf Deiner persönlichen Suche nach einer geeigneten Formation mit unserer langjährigen Erfahrung im Musikgeschäft professionell weiterhelfen.</p></br>
+	<p id="Fett">Wir können Dir hier folgendes Anbieten:</p></br></br>
+	
+	<p id="Rahmen">BAND – egal ob groß oder klein, wir finden die richtige Formation für Ihre Veranstaltung</p>
+	<p>Egal ob Sie eine große oder kleine Veranstaltung durchführen wollen, wir haben die richtige Musikformation für Sie angefangen von:</br>
+	<table id="Tabelle">
+			<tr><td><img id="Note" src="../Bilder/Note.png" alt="Note"></td><td>1 Mann Entertainer - 7 Mann Galabesetzung</td></tr>
+			<tr><td><img id="Note" src="../Bilder/Note.png" alt="Note"></td><td>3 - 5 Mann Jazzformation</td></tr>
+			<tr><td><img id="Note" src="../Bilder/Note.png" alt="Note"></td><td>7 - 14 Mann Blasorchester</td></tr>
+			<tr><td><img id="Note" src="../Bilder/Note.png" alt="Note"></td><td>15 Mann Bigband</td></tr>
+	</table></br>
+
+	<p id="Rahmen">EVENTMANAGEMENT – wir kümmern uns auch gerne um das Rahmenprogramm Ihres Events</p>
+	<table id="Tabelle">
+			<tr><td><img id="Note" src="../Bilder/Note.png" alt="Note"></td><td>Artisten</td></tr>
+			<tr><td><img id="Note" src="../Bilder/Note.png" alt="Note"></td><td>DJ-Service</td></tr>
+			<tr><td><img id="Note" src="../Bilder/Note.png" alt="Note"></td><td>Catering</td></tr>
+			<tr><td><img id="Note" src="../Bilder/Note.png" alt="Note"></td><td>Light- & Beamershows</td></tr>
+	</table></br>
+	
+	<p id="Rahmen">Konzeptionelle und technische Beratung – wir stellen unser langjähriges Know-How zur Verfügung</p>
+	<table id="Tabelle">
+			<tr><td><img id="Note" src="../Bilder/Note.png" alt="Note"></td><td>Beratung</td></tr>
+			<tr><td><img id="Note" src="../Bilder/Note.png" alt="Note"></td><td>Erfahrung</td></tr>
+			<tr><td><img id="Note" src="../Bilder/Note.png" alt="Note"></td><td>Professionalität</td></tr>
+			<tr><td><img id="Note" src="../Bilder/Note.png" alt="Note"></td><td>Diskretion</td></tr>
+	</table>
+	
+	<p id="Farbe">INTERESSIERT?</p>
+	<p id="Mittig">Dann Buchen Sie uns!</p>
 	</div>
-	<div id="Sidebar"><p>Newsletter </br>
-	Aktuelles</br></br>
+	
+	<div id="Sidebar"><p>	
+	<!--Newsletter-->
+	<span class="fett">Newsletter</span></br>
+	Abonnieren Sie unseren Newsletter, um die neuesten öffentliche Termine zu erhalten.
+	<form action="mailto:stefanie.eberhart@gmx.de" method="post">
+		Email-Adresse: <input type="text" name="Email-Adresse" value="" size="15" maxlength="30"></br>
+		Vorname: <input type="text" name="Vorname" value="" size="15" maxlength="30"></br>
+		Nachname: <input type="text" name="Nachname" value="" size="15" maxlength="30"></br>
+		<input type="Submit" name="Absenden" value="Absenden">
+	</form>
+	
+	<!--Impressum-->
 	<span class="fett"> Impressum</span></br>Mike and the Electronics</br>Michael Eberhart</br>Tel 07428/3417</br>
 	michael.eberhart1@gmx.de</br>Bei Buchungen gelten unsere Allgemeinen Geschäftsbedingungen:</br>Datei siehe Tanz- und Showband Prestige</p>
-	</p></div>
+	</p></br>
+	
+	<!--Aktuelles-->
+	<span class="fett">Aktuelles</span></br>Momentan gibt es noch keine Termine für 2017.</br></br></br>
+	</div>
 </body>
 </html>
