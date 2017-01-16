f38311fdef890c2df136293c2f4b745fed4a4bd7
StefanieEberhart94 <stefanie.eberhart@gmx.de>
2017-01-16 16:26:04 +0100

    Update java.php

--- htdocsNeu/java.php
+++ htdocsNeu/java.php
@@ -1,11 +1,8 @@
 <script>
 // Get the modal
 var modal = document.getElementById('myModal');
-
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('BioBild');
-//bisher wird nur MusikGrafik als PopUp angezeigt
-var img = document.getElementById('MusikGrafik');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
@@ -13,11 +10,25 @@ img.onclick = function(){
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
 }
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+</script>
+
+<script>
+var modal = document.getElementById('myModal');
+var img2 = document.getElementById('MusikGrafik');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img2.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
     modal.style.display = "none";
 }
