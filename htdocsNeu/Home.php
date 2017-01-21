2814b9727ba36a353792107b946fcbf9d371e256
StefanieEberhart94 <stefanie.eberhart@gmx.de>
2017-01-21 11:34:30 +0100

    Update Home.php

--- htdocsNeu/Home.php
+++ htdocsNeu/Home.php
@@ -7,6 +7,7 @@
 	<meta name="keywords" content="Mike and the Electronics, Musiker, Home, Eventmanager, Beratung, Alleinunterhalter, Band"/>
 	<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
 </head>
+
 <body>
 	<?php
 	include("nav.php");
@@ -46,51 +47,54 @@
 	<p id="Farbe">INTERESSIERT?</p>
 	<p id="Mittig">Dann Buchen Sie uns!</p>
 	<div id="Button"><a id="button" href="../Buchen/Buchen.php">Hier</br>Klicken!!!</a></div>
-		
-	<div id="Sidebar">	
-	<!--Newsletter-->
-	<p id="Seitentext"><span class="fett">Newsletter</span></br>
-	Abonnieren Sie unseren Newsletter, um die neuesten öffentliche Termine zu erhalten.</p>
-	<p id="Seitentext">E-Mail:</p>
 	
-	<?php
+<!--Hier wird "Abspeichern von Formulardate" aus der Vorlesung in angepasster Form für den Newsletter verwendet.-->
+<?php
 $mysql = new mysqli("localhost", "root", "", "musik2017");
 $error = "";
 if (isset($_POST['button'])) { 
    $mail = trim(isset($_POST['mail'])?$_POST['mail']:"");
-	  if (empty($mail) ) {
-      $error = "Bitte geben Sie ihr E-Mailadresse ein";} 
-	  else {   
+   if (empty($mail) ) {
+      $error = "Bitte alle Felder ausfüllen!";
+   } else {
       $insert = $mysql->prepare("INSERT INTO newsletter (url,
                 mail,zeitpunkt) VALUES (?,?,now())");
-      // Befüllen der Parameter ("ss" gibt an, dass alle zwei Parameter Strings sind.)
       $insert->bind_param("ss", $_SERVER['PHP_SELF'], $mail);
-      // Statement an die Datenbank schicken.
       $insert->execute();
-      // Ggf. Fehler in die $error-Variable übertragen.
-      if (!empty($insert->error)) $error = "DB ERROR: ".$insert->error;
-   }
+      if (!empty($insert->error)) $error = "DB ERROR: ".$insert->error;}
 }
 ?>
 
-<div id="newsletter">
+<div id="comments">
    <?php
-      // Falls es Fehler gibt, hier ausgeben.
-      if (!empty($error)) {echo ("<p>".$error."</p>");}
-	?>
+      if (!empty($error)) {
+                     echo ("<p>".$error."</p>");}
+   ?>
    <form method="post" action="">
-   <input id="leer" type="text" name="mail" length="20" /></br>
-   <input  id="AboButton" type="submit" name="button" value="Newsletter abonnieren!"/>
-   </form>
-   
+      E-Mail: <input type="text" name="mail" length="20" /><br/>      
+      <input type="submit" name="button" value="Kommentar absenden!"/>
+   </form></div>
 </div>
+
+	
+	<div id="Sidebar">	
+	<!--Newsletter-->
+	<p id="Seitentext"><span class="fett">Newsletter</span></br>
+	Abonnieren Sie unseren Newsletter, um die neuesten öffentliche Termine zu erhalten.
+	<form method="post" action="/formmail.php">
+	<p><label>E-Mail:</br><input id="Leer" type="text" name="Mail"></label></p>
+	<input id="AboButton" type="submit" value="Newsletter abonnieren">
+	</form></p>
+	
 	<!--Kontakt-->
 	<p id="Seitentext"><span class="fett"> Kontakt</span></br>
 	<span class="schwarz">Mike and the Electronics</span></br>Michael Eberhart</br>Tel. 07428/3417</br>
 	michael.eberhart1@gmx.de</br></br>Bei Buchungen gelten unsere</br> 
 	<a href="../agb.pdf"><img id="pdf" src="../Bilder/pdf.png" alt="Allgemeine Geschäftsbedingungen">Allgemeinen Geschäftsbedingungen.</a></p></br>
+	
 	<!--Aktuelle Informationen-->
 	<p id="Seitentext"><span class="fett">Aktuelles</span></br>Momentan sind noch keine Termine für 2017 bekannt.</p></br>
+	
 	<!--Impressum-->
 	<p id="Seitentext"><span class="fett">Design, Programmierung & Hosting:</span></br>Stefanie Eberhart und Bettina Haberstroh</p></br></br></div>
 </body>
