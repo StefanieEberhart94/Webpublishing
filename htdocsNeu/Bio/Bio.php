022d981881139a2dea7ba3e7326c89bcd7928878
StefanieEberhart94 <stefanie.eberhart@gmx.de>
2017-01-21 12:27:53 +0100

    Update Bio.php

--- htdocsNeu/Bio/Bio.php
+++ htdocsNeu/Bio/Bio.php
@@ -7,35 +7,39 @@
 	<meta name="keywords" content="Mike and the Electronics, Musiker, Michael Eberhart, Lebenslauf, Biographie"/>
 	<link type="text/css" rel="stylesheet" href="../stylesheet.css"/>
 </head>
+
 <body>
 	<?php
 	include("../nav.php");
 	?>
 	<div id="Ganz">
-	<img id="BioBild" src="../Bilder/Bewerbungsfoto.JPG" alt="Michael Eberhart" width="200" >
-
-<!-- The Modal -->
-<div id="myModal" class="modal">
-  <span class="close">&times;</span>
-  <img class="modal-content" id="img01">
-  <div id="caption"></div>
-</div>
-		<p id="Überschrift">Lebenslauf von Michael Eberhart</p>
-		<table id="Tabelle">
-			<tr><td>1963</td><td>Geburtsdatum</td></tr>
-			<tr><td>1971</td><td>Beginn der musikalischen Ausbildung in der SK Binsdorf als Trompeter</td></tr>
-			<tr><td>1977</td><td>Beginn mit E-Bass lernen+Mitspielen in verschiedenen Tanz & Unterhaltungsbands</td></tr>
-			<tr><td>1981</td><td>Mitglied im Kreisverbandsjugendorchester ZAK</br>Aushilfe als Trompeter in verschiedenen Vereinen überregional</td></tr>
-			<tr><td>1981-1983</td><td>Bassist in einer bekannten Bigband im Kreis Rottweil</td></tr>
-			<tr><td>1984 -1985</td><td>Mitglied als Trompeter im Luftwaffenmusikkorps ll in Karlsruhe</td></tr>
-			<tr><td>1985 -1988</td><td>Mitglied in verschiedenen Bands als Trompeter und Bassist</td></tr>
-			<tr><td>1992-2000</td><td>Trompeter/ Bassist in einer überregional bekannten Gala- und Tanzband</td></tr>
-			<tr><td>1996- heute</td><td>Dirigent in verschiedenen Blasorchester</td></tr>
-			<tr><td>seit 2012</td><td>Klavierunterricht</td></tr>
-		</table>
+	<img id="BioBild" src="../Bilder/Bewerbungsfoto.JPG" alt="Michael Eberhart" width="200">
+		
+	<p id="Überschrift">Lebenslauf von Michael Eberhart</p>
+	<table id="Tabelle">
+		<tr><td>1963</td><td>Geburtsdatum</td></tr>
+		<tr><td>1971</td><td>Beginn der musikalischen Ausbildung in der SK Binsdorf als Trompeter</td></tr>
+		<tr><td>1977</td><td>Beginn mit E-Bass lernen+Mitspielen in verschiedenen Tanz & Unterhaltungsbands</td></tr>
+		<tr><td>1981</td><td>Mitglied im Kreisverbandsjugendorchester ZAK</br>Aushilfe als Trompeter in verschiedenen Vereinen überregional</td></tr>
+		<tr><td>1981-1983</td><td>Bassist in einer bekannten Bigband im Kreis Rottweil</td></tr>
+		<tr><td>1984 -1985</td><td>Mitglied als Trompeter im Luftwaffenmusikkorps ll in Karlsruhe</td></tr>
+		<tr><td>1985 -1988</td><td>Mitglied in verschiedenen Bands als Trompeter und Bassist</td></tr>
+		<tr><td>1992-2000</td><td>Trompeter/ Bassist in einer überregional bekannten Gala- und Tanzband</td></tr>
+		<tr><td>1996- heute</td><td>Dirigent in verschiedenen Blasorchester</td></tr>
+		<tr><td>seit 2012</td><td>Klavierunterricht</td></tr>
+	</table>
 	</div>
-<?php
-	include("../java.php");
+	
+	<!-- The Modal -->
+	<div id="myModal" class="modal">
+		<span class="close">&times;</span>
+		<img class="modal-content" id="img01">
+		<div id="caption"></div>
+	</div>
+	
+	<?php
+		include("../Bilder/java.php");
 	?>
-	</body>
+	
+</body>
 </html>
